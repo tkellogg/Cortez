322fc601ba7393768282d85798203b85aa6af672
tkellogg <timothy.kellogg@gmail.com>
2013-01-11 23:23:32 -0700

    Changed syntax to config.Set(x => x.Prop).EqualTo(x => x.Prop)
    Mostly because it's more obvious if you're not already familiar. Also,
    I'm temporarily moving away from the deep specs, for the time being. This
    is hard...but it could be really cool.

--- Cortez.Tests/CortezTests.cs
+++ Cortez.Tests/CortezTests.cs
@@ -73,12 +73,13 @@ namespace Cortez.Tests
 
 		[Fact]
 		public void I_can_instruct_it_how_to_map_a_property() {
-			mh.Map<AggregateA, AggregateB>(config => config.Member (
-				(from, to) => from.Prop == new A{String = "from B: " + to.Prop}));
+			mh.Map<A, B>(config => config.Set(x => x.String).EqualTo(x => "from B: " + x.Int.ToString()));
+			mh.Map<AggregateA, AggregateB>();
+				//(from, to) => to.Prop == new A{String = "from B: " + from.Prop}));
 
 			var result = mh.Map<AggregateA, AggregateB>(new AggregateA{Id = 63, Prop=new A{String="bar"}});
 			Assert.NotNull (result.Prop);
-			Assert.Equal("from B: bar", result.Prop.String);
+			Assert.Equal("from B: 63", result.Prop.String);
 		}
 	}
 }
--- Cortez/Cortez.cs
+++ Cortez/Cortez.cs
@@ -40,34 +40,39 @@ namespace Cortez
 		public void Map<TIn, TOut> (Action<Configuration.IMapConfiguration<TIn, TOut>> expr) {
 			var config = new MapConfiguration<TIn, TOut>();
 			expr(config);
-			CreateMap<TIn, TOut>(config);
+			CreateMapAndCacheFullExpresion<TIn, TOut>(config);
 		}
 
 		public void Map<TIn, TOut>() {
-			CreateMap<TIn, TOut>(new MapConfiguration<TIn, TOut>());
+			CreateMapAndCacheFullExpresion<TIn, TOut>(new MapConfiguration<TIn, TOut>());
 		}
 
-		void CreateMap<TIn, TOut>(MapConfiguration<TIn, TOut> config) {
+		void CreateMapAndCacheFullExpresion<TIn, TOut>(MapConfiguration<TIn, TOut> config) {
 			var key = Tuple.Create (typeof (TIn), typeof (TOut));
 			if (_functions.ContainsKey(key)) return;
 
-			var fnExpr = CreateMap(typeof (TIn), typeof (TOut), config);
+			var fnExpr = CreateMapAndCacheFullExpresion(typeof (TIn), typeof (TOut), config);
 			_functions[key] = fnExpr.Compile();
 		}
 
-		LambdaExpression CreateMap(Type typeIn, Type typeOut, IPropertyMap properties) {
+		/// <summary>
+		/// Creates a `new TSource { Prop1 = ..., Prop2 = ..., ... }` expression. For each
+		/// property initialization it calls GetPropertyValue, which can nest a lot deeper.
+		/// </summary>
+		LambdaExpression CreateMapAndCacheFullExpresion(Type typeIn, Type typeOut, IPropertyMap properties) {
 			var propMatches = from tin in typeIn.GetProperties()
 							  join tout in typeOut.GetProperties() on tin.Name equals tout.Name
 							  select Tuple.Create(tin, tout);
 
 			var inParam = Expression.Parameter (typeIn, "input");
 
+			// As a Func because we need to reuse it later with different parameters
 			Func<Expression, Expression> getInitExpr = param => 
 			{
-				var assignments = propMatches.Select (pair => Expression.Bind (
-					pair.Item2, 
-					GetPropertyValue(param, pair.Item1, pair.Item2.PropertyType, properties)
-				)).ToArray();
+				var assignments = propMatches.Select (props => {
+					var val = GetPropertyValue(param, props.Item1, props.Item2, properties);
+					return Expression.Bind (props.Item2, val);
+				}).ToArray();
 
 				return Expression.MemberInit(
 					Expression.New (typeOut),
@@ -80,17 +85,26 @@ namespace Cortez
 			return Expression.Lambda(initExpr, inParam);
 		}
 
-		Expression GetPropertyValue (Expression param, PropertyInfo source, Type destType, IPropertyMap properties)
+		/// <summary>
+		/// Gets an expression representing the value of a property. This could be a simple
+		/// member access, or as complicated as another mapping function.
+		/// </summary>
+		/// <param name='param'>The object that <c>propIn</c> belongs to</param>
+		/// <param name='propIn'>The property that data is flowing from</param>
+		/// <param name='typeOut'>The property type of the destination</param>
+		/// <param name='properties'></param>
+		Expression GetPropertyValue (Expression param, PropertyInfo propIn, PropertyInfo propOut, IPropertyMap properties)
 		{
-			var memberAccess = Expression.MakeMemberAccess (param, source);
+			var typeOut = propOut.PropertyType;
+			var memberAccess = Expression.MakeMemberAccess (param, propIn);
 			Func<Expression, Expression> fn;
-			if (!properties.Maps.TryGetValue (source, out fn)) {
-				if (source.PropertyType.Namespace.StartsWith ("System") && destType.Namespace.StartsWith ("System"))
+			if (!properties.Maps.TryGetValue (propOut, out fn)) {
+				if (propIn.PropertyType.Namespace.StartsWith ("System") && typeOut.Namespace.StartsWith ("System"))
 					return memberAccess;
 
-				var key = Tuple.Create (source.PropertyType, destType);
+				var key = Tuple.Create (propIn.PropertyType, typeOut);
 				if (!_expressions.TryGetValue (key, out fn)) {
-					CreateMap (source.PropertyType, destType, properties);
+					CreateMapAndCacheFullExpresion (propIn.PropertyType, typeOut, properties);
 					fn = _expressions [key];
 				}
 			}
@@ -111,14 +125,55 @@ namespace Cortez
 
 				var propertyInfo = (PropertyInfo) memberExpr.Member;
 				var body = binExpr.Right;
-				Maps[propertyInfo] = param => new SubstituteVisitor(assignment.Parameters[1], param).Visit(body);
+				Maps[propertyInfo] = param => body;
 				return this;
 			}
 
+			public Configuration.IMapPropertyValue<TSource, TDest, TProperty> Set<TProperty>(Expression<Func<TDest, TProperty>> propertySelector) {
+				return new MapPropertyValue<TProperty>(this, propertySelector);
+			}
+
 			Dictionary<PropertyInfo, Func<Expression, Expression>> _maps = new Dictionary<PropertyInfo, Func<Expression, Expression>>();
+			/// <value>
+			/// The Func processes 1 parameter (a member access) and returns 
+			/// </value>
 			public Dictionary<PropertyInfo, Func<Expression, Expression>> Maps { get { return _maps; } }
+
+			class MapPropertyValue<TProperty> : Configuration.IMapPropertyValue<TSource, TDest, TProperty>
+			{
+				MapConfiguration<TSource, TDest> _config;
+				public PropertyInfo Destination { get; set; }
+
+				public MapPropertyValue(MapConfiguration<TSource, TDest> config, Expression<Func<TDest, TProperty>> destinationSelector) {
+					_config = config;
+
+					var memberExpr = destinationSelector.Body as MemberExpression;
+					if (memberExpr == null) return;
+					Destination = (PropertyInfo) memberExpr.Member;
+				}
+
+				public Configuration.IMapConfiguration<TSource,TDest> EqualTo(Expression<Func<TSource, TProperty>> sourceSelector) {
+					_config.Maps[Destination] = to => {
+						to = FindParameter (to);
+						return Substitute(sourceSelector.Body, sourceSelector.Parameters[0], to);
+					};
+					return _config;
+				}
+			}
 		}
 
+		static ParameterExpression FindParameter(Expression expr) {
+			while (expr is MemberExpression) {
+				expr = ((MemberExpression)expr).Expression;
+			}
+			return expr as ParameterExpression;
+		}
+
+		static Expression Substitute(Expression @using, Expression find, Expression replaceWith) {
+			return new SubstituteVisitor(find, replaceWith).Visit(@using);
+		}
+
+		// Not used. I may not use yet yet...
 		class SubstituteVisitor : ExpressionVisitor {
 			public readonly Expression From, To;
 			public SubstituteVisitor(Expression from, Expression to) {
@@ -128,7 +183,8 @@ namespace Cortez
 
 			public override Expression Visit (Expression node) {
 				if (node == From) return To;
-				return base.Visit (node);
+				var ret = base.Visit (node);
+				return ret;
 			}
 		}
 	}
@@ -137,7 +193,13 @@ namespace Cortez
 	{
 		public interface IMapConfiguration<TSource, TDest>
 		{
-			IMapConfiguration<TSource, TDest> Member(Expression<Func<TSource, TDest, bool>> assignmentSelector);
+			//IMapConfiguration<TSource, TDest> Member(Expression<Func<TSource, TDest, bool>> assignmentSelector);
+			IMapPropertyValue<TSource, TDest, TProperty> Set<TProperty>(Expression<Func<TDest, TProperty>> propertySelector);
+		}
+
+		public interface IMapPropertyValue<TSource, TDest, TProperty> 
+		{
+			IMapConfiguration<TSource,TDest> EqualTo(Expression<Func<TSource, TProperty>> valueSelector);
 		}
 	}
 }
